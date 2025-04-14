dc0d7b0d34fcfbd732e1b8c7521e508bf65c8552
Gabriel Ferreira Carelli <100717337+GabrielCarelli@users.noreply.github.com>
2025-04-14 15:07:31 -0300

    02

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,12 @@
 import Image from "next/image";
 import UntitledPng from "../../public/Untitled.png";
 import Socials from "@/components/socials";
+import { Projetos } from "@/data/Projetos";
+import Link from "next/link";
+import { ArrowRight } from "react-feather";
 export default function Home() {
   return (
+    <>
     <div className="flex flex-col-reverse md:flex-row md:justify-between md:space-y-0">
       <div className="mt-8 flex flex-col justify-center space-y-4 md:mt-0 md:w-2/3">
         <h1 className="will-change-opacity text-5xl font-bold will-change-transform">
@@ -15,7 +19,7 @@ export default function Home() {
         </h2>
         <p className="text-md will-change-opacity text-lg text-gray-300 will-change-transform">
           I&apos;m a full-stack web developer with a passion for exploring new technologies—currently
-          diving into Rust, Go, Web3, and advanced backend development. 
+          diving into Rust, Go, Web3, and advanced backend development.
           Outside of coding, I enjoy writing, which naturally led me to create and maintain my own
           blog where I share insights, experiments, and ideas.
         </p>
@@ -24,9 +28,41 @@ export default function Home() {
       <div className="will-change-opacity relative h-32 w-32 overflow-hidden rounded-full will-change-transform">
       </div>
       <div className="will-change-opacity relative h-32 w-32 overflow-hidden rounded-full will-change-transform">
-        <Image 
-          src={UntitledPng} alt={"photo"}  />
+        <Image
+          src={UntitledPng} alt={"photo"} />
       </div>
+
     </div>
+    <div className="my-16 flex flex-col">
+        <h2 className="mb-8 text-3xl font-bold">
+          Projects
+        </h2>
+        <div className="flex flex-col space-y-8">
+          {Projetos.slice(0, 3).map(({index, project, name, url}) =>(
+            <div key={index} className="group relative rounded-xl border-[1px] border-primary bg-secondary/5 p-4 transition duration-200 hover:border-accent md:hover:scale-[1.01]">
+              <Link 
+              href={url}
+              className="flex flex-col space-y-8 md:flex-row md:space-y-0 md:space-x-4"
+              >
+                <div className="flex-col space-y-2">
+                  <h3 className="flex flex-row items-center space-x-2 text-lg font-semibold">
+                    {name}
+                  </h3>
+                  <p className="text-md text-gray-300">{project}
+                  </p>
+                </div>
+              </Link>
+            </div>
+          ))}
+        </div>
+        <Link
+          href="/projects"
+          className="group mt-8 flex items-center justify-start space-x-2 text-xl font-medium"
+        >
+          <span>View All Projects</span>
+          <ArrowRight className="h-4 w-4 transition duration-200 group-hover:translate-x-1" />
+        </Link>
+      </div>
+      </>
   );
 }
---
+++ src/data/Projetos.ts
@@ -0,0 +1,20 @@
+export const Projetos = [
+  {
+    index: "1",
+    project: "A modern, web-based CRM tailored for car dealerships and internal sales teams. Built with Next.js, Tailwind CSS, and SQLite, this system helps manage sales tracking, client interests, and seller records — all secured with Clerk authentication.",
+    name: "Car Dealership CRM",
+    url: "https://github.com/GabrielCarelli/projeto_crm",
+  },
+  {
+    index: "2",
+    project: "A sleek, modern reinterpretation of the classic Pokédex, built with Next.js to explore efficient API data fetching, state management, and dynamic routing. This project serves as a hands-on study in handling real-time data from the PokéAPI, implementing responsive UI/UX design, and optimizing performance in a React-based application.",
+    name: "Pokédéx NEXT.js",
+    url: "https://github.com/GabrielCarelli/nextjs-pokedex",
+  },
+  {
+    index: "3",
+    project: "This project marked my introduction to Next.js—a hands-on exploration of the framework’s core features and unique capabilities. Built as a learning experiment, it helped me grasp key concepts like server-side rendering, API integration, and dynamic routing, while uncovering what sets Next.js apart from traditional React.",
+    name: "Football Team News Blog",
+    url: "https://github.com/GabrielCarelli/BlogSunderland",
+  },
+];
